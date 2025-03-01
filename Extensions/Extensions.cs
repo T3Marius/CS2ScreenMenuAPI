6cf484c2f03a6ab871a7eeecb7b6ee9ca4a40b63
T3Marius <168598779+T3Marius@users.noreply.github.com>
2025-03-01 05:17:51 +0200

    feat: added Freeze and Unfreeze player for ScrollableMenu

--- Extensions/Extensions.cs
+++ Extensions/Extensions.cs
@@ -3,7 +3,11 @@ using System.Drawing;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Memory;
+using CounterStrikeSharp.API.Modules.Utils;
+using CS2ScreenMenuAPI.Internal;
 
 namespace CS2ScreenMenuAPI.Extensions
 {
@@ -38,6 +42,51 @@ namespace CS2ScreenMenuAPI.Extensions
                 ? value.Name
                 : ColorTranslator.ToHtml(value);
             writer.WriteStringValue(colorString);
+        }
+    }
+    public static class Player
+    {
+        public static void Freeze(this CCSPlayerController player)
+        {
+            CCSPlayerPawn? pawn = player.PlayerPawn.Value;
+            if (pawn != null)
+            {
+                pawn.ChangeMoveType(MoveType_t.MOVETYPE_OBSOLETE);
+            }
+        }
+        public static void Unfreeze(this CCSPlayerController player)
+        {
+            CCSPlayerPawn? pawn = player.PlayerPawn.Value;
+            if (pawn != null)
+            {
+                pawn.ChangeMoveType(MoveType_t.MOVETYPE_WALK);
+            }
+        }
+        public static void ChangeMoveType(this CBasePlayerPawn pawn, MoveType_t movetype)
+        {
+            pawn.MoveType = movetype;
+            Schema.SetSchemaValue(pawn.Handle, "CBaseEntity", "m_nActualMoveType", movetype);
+            Utilities.SetStateChanged(pawn, "CBaseEntity", "m_MoveType");
+        }
+    }
+    public static class WorldTextExtensions
+    {
+        public static Vector GetPosition(this CPointWorldText entity)
+        {
+            if (WorldTextManager.EntityTransforms.TryGetValue(entity.Index, out var transform))
+            {
+                return transform.Position;
+            }
+            return new Vector();
+        }
+
+        public static QAngle GetAngles(this CPointWorldText entity)
+        {
+            if (WorldTextManager.EntityTransforms.TryGetValue(entity.Index, out var transform))
+            {
+                return transform.Angles;
+            }
+            return new QAngle();
         }
     }
 }
